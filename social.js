5471cbc1ed2f1bb1f55620d36e59667f0d22139b
Harshad Mane <harshad.mane@pubmatic.com>
2026-02-07 12:10:45 -0800

    Create social.js

---
+++ social.js
@@ -0,0 +1,10 @@
+var GoogleReviewWidget = {
+    rating: 5,
+    reviews: 58,
+    url: "https://maps.app.goo.gl/g7Q5voc2tPtrQvqN8"
+}
+
+var InstagramWidget = {
+    followers: "101",
+    url: "https://www.instagram.com/dehaati_indiancuisine/"
+}
